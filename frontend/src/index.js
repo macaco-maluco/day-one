66f5203674b8fffa873675c5b115c6ae39b1da2e
Fernando Via Canel <fernando.canel@klarna.com>
2016-11-12 11:23:21 +0100

    Rendered star field

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react'
-// import {render} from 'react-dom'
+import React from 'react'
+import {render} from 'react-dom'
 import noiseMatrix from 'noise-matrix'
 import getMyPosition from 'get-my-position'
 
@@ -14,9 +14,20 @@ const isStar = (x) => x > starCutFactor
 const starredMatrix = matrix.map((x) => [
   ...x,
   isStar(x[2])
-])
+]).filter(([_, _1, _2, x]) => x)
 
-const amountOfStars = starredMatrix.filter(([_, _1, _2, x]) => x).length
+const amountOfStars = starredMatrix.length
 
 console.table(starredMatrix)
 console.log('amount', amountOfStars)
+
+render(
+  <svg width={windowSize[0]} height={windowSize[1]}>
+    {starredMatrix.map(([x, y, ..._]) => <circle
+      cx={x}
+      cy={y}
+      r={5}
+    />)}
+  </svg>,
+  document.getElementById('root')
+)
