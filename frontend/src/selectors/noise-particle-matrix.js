c97cec987904b0dbdb4062975066f233cde1e7aa
Fernando Via Canel <fernando.canel@klarna.com>
2016-11-13 14:55:39 +0100

    Fix

--- frontend/src/selectors/noise-particle-matrix.js
+++ frontend/src/selectors/noise-particle-matrix.js
@@ -1,51 +1,45 @@
-import SimplexNoise from 'simplex-noise'
+import Alea from 'alea'
 import {range} from 'ramda'
 import {GRID_PARTICLES} from 'constants'
-import seedableRandom from 'helpers/seedable-random'
 
 const {floor, ceil} = Math
 
-const iterativeSeedableRandom = (seed) => {
-  let counter = 0
-  return () => {
-    return seedableRandom(seed, counter++)
-  }
+const random = (seed) => {
+  const r = new Alea(seed)
+
+  return r()
 }
 
-export default (seed) => {
-  const simplex = new SimplexNoise(iterativeSeedableRandom(seed))
+export default (seed) => (universe) => {
+  const { viewport, cameraPositionStart, cameraPosition } = universe
+  const viewportInGrid = viewport.map((x) => ceil(x / GRID_PARTICLES))
 
-  return (universe) => {
-    const { viewport, cameraPositionStart, cameraPosition } = universe
-    const viewportInGrid = viewport.map((x) => ceil(x / GRID_PARTICLES))
+  const myPositionInTheGrid = cameraPositionStart
+    .map((x, i) => x - cameraPosition[i])
+    .map((x) => floor(x / GRID_PARTICLES))
 
-    const myPositionInTheGrid = cameraPositionStart
-      .map((x, i) => x - cameraPosition[i])
-      .map((x) => floor(x / GRID_PARTICLES))
+  const ranges = viewportInGrid.map((x, i) => [
+    myPositionInTheGrid[i] - ceil(x / 2) - 1,
+    myPositionInTheGrid[i] + ceil(x / 2) + 1
+  ])
 
-    const ranges = viewportInGrid.map((x, i) => [
-      myPositionInTheGrid[i] - ceil(x / 2) - 1,
-      myPositionInTheGrid[i] + ceil(x / 2) + 1
-    ])
+  const xRange = range(ranges[0][0], ranges[0][1])
 
-    const xRange = range(ranges[0][0], ranges[0][1])
+  const yRange = range(ranges[1][0], ranges[1][1])
 
-    const yRange = range(ranges[1][0], ranges[1][1])
+  const matrix = xRange.map((x) => yRange.map((y) => [x, y])).reduce((a, b) => a.concat(b), [])
 
-    const matrix = xRange.map((x) => yRange.map((y) => [x, y])).reduce((a, b) => a.concat(b), [])
+  const gridToDots = (dot) =>
+    dot
+      .map((x) => x * GRID_PARTICLES)
 
-    const gridToDots = (dot) =>
-      dot
-        .map((x) => x * GRID_PARTICLES)
+  const particleMatrix = matrix.map((dot) => [
+    ...(gridToDots(dot)),
+    random(seed + 'back' + dot.join('.'))
+  ])
 
-    const particleMatrix = matrix.map((dot) => [
-      ...(gridToDots(dot)),
-      simplex.noise2D(...dot)
-    ])
-
-    return {
-      ...universe,
-      particleMatrix
-    }
+  return {
+    ...universe,
+    particleMatrix
   }
 }
