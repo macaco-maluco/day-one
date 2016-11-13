ed1a62d1972c08a4065720dfd45db05f952c3eda
Paulo Ragonha <paulo@ragonha.me>
2016-11-13 16:47:30 +0100

    Animate the accretion disk into the solar system

--- frontend/src/components/solar-system/star.js
+++ frontend/src/components/solar-system/star.js
@@ -1,39 +1,67 @@
 import React from 'react'
+import {Motion, spring} from 'react-motion'
+import {SOLAR_SYSTEM_STAGES} from 'constants'
 
-export default function ({position, radius, opacity, type, timeLeft}) {
+export default function ({pixelPosition, radius, type, stage}) {
+  return <g>
+    <MainSequence pixelPosition={pixelPosition} radius={radius} type={type} />
+    <AccretionDisk pixelPosition={pixelPosition} radius={radius * 5} expanded={stage === SOLAR_SYSTEM_STAGES.ACCRETION_DISK} />
+  </g>
+}
+
+function AccretionDisk ({pixelPosition, expanded, radius}) {
+  return <Motion
+    defaultStyle={{
+      radius: expanded ? radius : 0
+    }}
+    style={{
+      radius: spring(expanded ? radius : 0)
+    }}
+    >
+    {(style) => (
+      <circle
+        cx={pixelPosition[0]}
+        cy={pixelPosition[1]}
+        r={style.radius}
+        fill='#5e3734'
+      />
+    )}
+  </Motion>
+}
+
+function MainSequence ({pixelPosition, radius, type}) {
   return <g className={`star ${type}`}>
     <circle
       className={`star-part glow1 ${type}`}
-      cx={position[0]}
-      cy={position[1]}
+      cx={pixelPosition[0]}
+      cy={pixelPosition[1]}
       r={radius + 5}
       stroke='none'
     />
     <circle
       className={`star-part glow2 ${type}`}
-      cx={position[0]}
-      cy={position[1]}
+      cx={pixelPosition[0]}
+      cy={pixelPosition[1]}
       r={radius + 8}
       stroke='none'
     />
     <circle
       className={`star-part center ${type}`}
-      cx={position[0]}
-      cy={position[1]}
+      cx={pixelPosition[0]}
+      cy={pixelPosition[1]}
       r={radius}
-      opacity={opacity}
       stroke='none'
     />
-    <StarMarks noise={timeLeft} position={position} radius={radius} />
+    <StarMarks pixelPosition={pixelPosition} radius={radius} />
   </g>
 }
 
-function StarMarks ({position, timeleft, radius}) {
+function StarMarks ({pixelPosition, radius}) {
   const size = radius / 3
   return <g className='mark'>
     <circle
-      cx={position[0] + size}
-      cy={position[1] - size}
+      cx={pixelPosition[0] + size}
+      cy={pixelPosition[1] - size}
       r={size}
       stroke='none'
     />
