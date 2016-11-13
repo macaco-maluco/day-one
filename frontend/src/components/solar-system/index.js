a97e8c5f4039beed3628d2f5696d7c141b2f2f48
Paulo Ragonha <paulo@ragonha.me>
2016-11-13 17:04:03 +0100

    Make planets pop in!

--- frontend/src/components/solar-system/index.js
+++ frontend/src/components/solar-system/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Star from './star'
 import {SOLAR_SYSTEM_STAGES} from 'constants'
+import Star from './star'
+import Planets from './planets'
 
 export default function solarSystems ({
   name,
@@ -19,35 +20,7 @@ export default function solarSystems ({
     <g
       style={{cursor: 'pointer', WebkitTapHighlightColor: 'rgba(0,0,0,0)'}}
       onClick={(e) => { e.stopPropagation(); onClickStar(e) }}>
-      {stage === SOLAR_SYSTEM_STAGES.MAIN_SEQUENCE && planets.map((p, index) => {
-        return <g
-          key={index}
-          onClick={(e) => { e.stopPropagation(); onClickPlanet(index) }}
-          style={{
-            transform: `rotate(${p.translation}rad)`,
-            transformOrigin: `${pixelPosition[0]}px ${pixelPosition[1]}px`,
-            transition: 'transform 1s linear'
-          }}>
-          <circle
-            className={`planet-orbit ${p.material}`}
-            cx={pixelPosition[0]}
-            cy={pixelPosition[1]}
-            r={p.orbit}
-            fill='none'
-            opacity={0.3}
-            stroke='#97005d'
-            strokeWidth='1'
-          />
-          <circle
-            className={`planet ${p.material}`}
-            cx={pixelPosition[0] + p.orbit}
-            cy={pixelPosition[1]}
-            r={p.radius}
-            fill={colors[p.material]}
-            stroke='none'
-          />
-        </g>
-      })}
+      <Planets pixelPosition={pixelPosition} planets={planets} onClickPlanet={onClickPlanet} stage={stage} />
       <Star type={starType} stage={stage} pixelPosition={pixelPosition} radius={starRadius} timeLeft={timeLeft} opacity={timeLeft / lifespan} />
       {stage === SOLAR_SYSTEM_STAGES.MAIN_SEQUENCE && <text
         style={{ fill: '#d2cfff', textTransform: 'uppercase' }}
@@ -59,13 +32,4 @@ export default function solarSystems ({
       </text>}
     </g>
   )
-}
-
-const colors = {
-  'water': '#3fb9dd',
-  'plutonium': '#97005d',
-  'hydrogen': '#dcbbaf',
-  'iron': '#ffffff',
-  'carbon': '#131139',
-  'titanium': '#3feeac'
 }
