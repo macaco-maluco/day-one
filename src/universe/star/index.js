50944470873b6f166bcc30fcadc6053b3fdd6e7e
Paulo Ragonha <paulo@ragonha.me>
2017-01-07 14:39:14 +0100

    Create stages function 🤕

--- src/universe/star/index.js
+++ src/universe/star/index.js
@@ -4,12 +4,14 @@ import birth from './birth'
 import lifespan from './lifespan'
 import name from './name'
 import type from './type'
+import stages from './stages'
 
 const star = (noise) => ({
   noise
 })
 
 export default (constants) => compose(
+  stages(constants),
   radius(constants),
   type(constants),
   birth(constants),
---
+++ src/universe/star/stages.js
@@ -0,0 +1,5 @@
+export default (constants) => (star) => {
+  return {
+    ...star
+  }
+}
