7737bcb365eef527eaf1b2349527389eac2f06f1
Paulo Ragonha <paulo@ragonha.me>
2017-01-07 14:18:37 +0100

    Scale the playground so that we see more of the structure 🗼

--- src/playground.js
+++ src/playground.js
@@ -4,14 +4,18 @@ import Quadrant from 'components/quadrant'
 import generateQuadrant from 'universe/quadrant'
 import 'styles.scss'
 
+const scale = 4
+const width = window.innerWidth * scale
+const height = window.innerHeight * scale
+
 const noise = 0.2
-const size = [window.innerWidth, window.innerHeight]
+const size = [width, height]
 const coordinates = [0, 0]
 
 const quadrant = generateQuadrant({})(noise, size, coordinates)
 
 render(
-  <svg width='100vw' height='100vh' style={{ background: 'black' }}>
+  <svg width='100vw' height='100vh' viewBox={`0 0 ${width} ${height}`} style={{ background: 'black' }}>
     <Quadrant {...quadrant} />
   </svg>
   , document.getElementById('root')
